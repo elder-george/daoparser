738796647f9a42198102886c849c32b2f4ce5317
Yuri Korchyomkin <yuri.korchyomkin@googlemail.com>
2013-02-20 00:45:01 -0800

    First working version: can parse entities with single line hierarchy; Will probably fail when hierarchy becomes complex.

--- SPParse.cs
+++ SPParse.cs
@@ -4,59 +4,113 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.IO;
 using System.Data;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Data.SQLite;
 
+
+[DataContract]
 public class User{
+    [DataMember]
     public int UserId{get;set;}
+    [DataMember]
     public string Name{get;set;}
+    [DataMember]
     public Post[] Posts{get;set;}
 }
-
+[DataContract]
 public class Post{
+    [DataMember]
     public int PostId{get;set;}
+    [DataMember]
     public int UserId{get;set;}
+    [DataMember]
     public string Title{get;set;}
+    [DataMember]
     public string Body{get;set;}
 }
 
-public class ParserSettings<T>{
+public interface IParserSettings{
+    IEnumerable<IEntityReader> CreateReaders();
+}
+
+public class ParserSettings<T>: IParserSettings where T:new(){
+    IDictionary<PropertyInfo, IIncludeResolver<T>> _resolvers = new Dictionary<PropertyInfo, IIncludeResolver<T>>();
+
+    IList<IParserSettings> _includes = new List<IParserSettings>();
+    int _resultSetIdx;
+
+    public ParserSettings(int resultSetIdx){
+        _resultSetIdx  = resultSetIdx;
+    }
+
     public ParserSettings<T> IncludeList<U, TKey>(
-        Expression<Func<T, IEnumerable<U>>> listProp, 
-        Expression<Func<T, TKey>> foreign,
-        Expression<Func<U, TKey>> primary, 
-        int resultSetIdx){
-            // TODO
+            Expression<Func<T, U[]>> listProp, 
+            Expression<Func<T, TKey>> foreign,
+            Expression<Func<U, TKey>> primary, 
+            int resultSetIdx) where U:new(){
+        var member = (MemberExpression)listProp.Body;
+        var prop = (PropertyInfo)member.Member;
+        _resolvers[prop] = CreateResolver(listProp, foreign, primary);
+        _includes.Add(new ParserSettings<U>(_resultSetIdx+1)); //TODO: if they go nested then overlaps are possible!!!
         return this;
     }
 
-    public IList<IEntityReader> CreateReaders(){
-        return null;
-    }
-}
-
-public interface IEntityReader{
-    void ReadRow(IDataReader rdr);
-    void ResolveIncludes(IList<IEntityReader> resultSets);
-    IEnumerable<Object> Rows{get;}
-}
-
-interface IPropertySetter<T>{
-    void SetValue(T target, IDataReader reader);
-}
-
-class PropertySetter<T, U>{
-    Action<T, U> _set;
-    Func<object, U> _convert;
-    string _propertyName;
-
-    public PropertySetter(Action<T, U> set, Func<object, U> convert, string propertyName){
-        _set = set;
-        _convert = convert;
-        _propertyName = propertyName;
+    public IEnumerable<IEntityReader> CreateReaders(){
+        var readers = new List<IEntityReader>();
+        var setters = new List<IPropertySetter<T>>();
+        var resolvers = new List<IIncludeResolver<T>>();
+        foreach(var prop in typeof(T).GetProperties()){
+            if (!_resolvers.ContainsKey(prop)){
+                setters.Add(CreateSetter(prop));
+            } else {
+                resolvers.Add(_resolvers[prop]);
+            }
+        }
+        readers.Add(new EntityReader<T>(resolvers, setters, _resultSetIdx));
+        foreach(var include in _includes){
+            readers.AddRange(include.CreateReaders());
+        }
+        return readers;
     }
 
-    public void SetValue(T entity, IDataReader reader){
-        _set(entity, _convert(reader[_propertyName]));
+    IIncludeResolver<T> CreateResolver<U, TKey>(
+            Expression<Func<T, U[]>> listProp, 
+            Expression<Func<T, TKey>> foreign,
+            Expression<Func<U, TKey>> primary){
+        
+        var param = Expression.Parameter(typeof(U[]));
+        return new IncludeResolver<T, U, TKey>(
+            Expression.Lambda<Action<T, U[]>>(
+                Expression.Assign(listProp.Body, param), 
+                new ParameterExpression[]{listProp.Parameters[0], param}).Compile(),
+            foreign.Compile(),
+            primary.Compile(),
+            _resultSetIdx+1);
+    }
+
+
+    IPropertySetter<T> CreateSetter(PropertyInfo prop){
+        var propType = prop.PropertyType;
+        var actionType = typeof(Action<,>).MakeGenericType(typeof(T), propType);
+        var set = Delegate.CreateDelegate(actionType,prop.GetSetMethod());
+        var convertType = typeof(Func<,>).MakeGenericType(typeof(object), propType);
+        Delegate convert;
+
+        if (propType.IsValueType && propType.IsGenericType && propType.GetGenericTypeDefinition() == typeof(Nullable<>)){
+            convert = Delegate.CreateDelegate(convertType, this.GetType().GetMethod("ConvertNullable", BindingFlags.Static));
+        } else {
+            convert = Delegate.CreateDelegate(convertType, 
+                this.GetType().GetMethod("Convert", BindingFlags.Static|BindingFlags.Public)
+                    .MakeGenericMethod(propType));
+        }
+        return (IPropertySetter<T>)Activator.CreateInstance(
+                    typeof(PropertySetter<,>).MakeGenericType(typeof(T), propType),
+                    new object[]{ set, convert, prop.Name}, 
+                    null);
+            
     }
 
     public static V? ConvertNullable<V>(object val) where V:struct{
@@ -68,6 +122,35 @@ class PropertySetter<T, U>{
         if (val == DBNull.Value) return default(V);
         return (V)val;
     }
+
+}
+
+public interface IEntityReader{
+    int ResultSetIndex{get;}
+    void ReadRow(IDataReader rdr);
+    void ResolveIncludes(IList<IEntityReader> resultSets);
+    IEnumerable<Object> Rows{get;}
+}
+
+interface IPropertySetter<T>{
+    void SetValue(T target, IDataReader reader);
+}
+
+class PropertySetter<T, U>: IPropertySetter<T>{
+    Action<T, U> _set;
+    Func<object, U> _convert;
+    string _propertyName;
+
+    public PropertySetter(Action<T, U> set, Func<object, U> convert, string propertyName){
+        _set = set;
+        _convert = convert;
+        _propertyName = propertyName;
+    }
+
+    public void SetValue(T entity, IDataReader reader){
+        Console.WriteLine("Reading property {0}", _propertyName);
+        _set(entity, _convert(reader[_propertyName]));
+    }
 }
 
 interface IIncludeResolver<T>{
@@ -76,11 +159,11 @@ interface IIncludeResolver<T>{
 
 class IncludeResolver<T, U, TKey>:IIncludeResolver<T>{
     int _resultSetIdx;
-    Action<T, IEnumerable<U>> _set;
+    Action<T, U[]> _set;
     Func<T, TKey> _foreignSelector;
     Func<U, TKey> _primarySelector;
 
-    public IncludeResolver(Action<T, IEnumerable<U>> set,
+    public IncludeResolver(Action<T, U[]> set,
                             Func<T, TKey> foreignSelector,
                             Func<U, TKey> primarySelector,
                             int resultSetIdx){
@@ -98,7 +181,7 @@ class IncludeResolver<T, U, TKey>:IIncludeResolver<T>{
         foreach(var e in entities){
             var foreign = _foreignSelector(e);
             if (childByKey.Contains(foreign)){
-                _set(e, childByKey[foreign]);
+                _set(e, childByKey[foreign].ToArray());
             }
         }
     }
@@ -110,10 +193,14 @@ class EntityReader<T>:IEntityReader where T: new(){
     IList<IIncludeResolver<T>> _resolvers;
     IList<IPropertySetter<T>> _setters;
 
+    public int ResultSetIndex{get; private set;}
+
     public EntityReader(IList<IIncludeResolver<T>> resolvers,
-                        IList<IPropertySetter<T>> setters){
+                        IList<IPropertySetter<T>> setters, 
+                        int resultSetIndex){
         _resolvers = resolvers;
         _setters = setters;
+        ResultSetIndex = resultSetIndex;
     }
 
 
@@ -122,6 +209,7 @@ class EntityReader<T>:IEntityReader where T: new(){
         foreach(var setter in _setters){
             setter.SetValue(entity, rdr);
         }
+        _rows.Add(entity);
     }
     public void ResolveIncludes(IList<IEntityReader> resultSets){
         foreach(var resolver in _resolvers){
@@ -132,13 +220,13 @@ class EntityReader<T>:IEntityReader where T: new(){
 }
 
 
-public class Parser<TRoot>{
+public class Parser<TRoot> where TRoot:new(){
     IList<IEntityReader> _entityReaders;
 
     public Parser(Action<ParserSettings<TRoot>> cfg){
-        var ps = new ParserSettings<TRoot>();
+        var ps = new ParserSettings<TRoot>(0);
         cfg(ps);
-        _entityReaders = ps.CreateReaders();
+        _entityReaders = ps.CreateReaders().ToArray();
     }
 
     public IEnumerable<TRoot> Parse(IDataReader rdr){
@@ -195,8 +283,11 @@ SELECT PostId, UserId, title, body FROM Post;
                                     _.IncludeList(u => u.Posts,
                                                   u => u.UserId,
                                                   p => p.UserId, 1));
-                var usersWithPosts = parser.Parse(rdr);
-//                new DataContractJsonSerializer(typeof(User)).WriteObject(u
+                var usersWithPosts = parser.Parse(rdr).ToArray();
+                using (var str = new MemoryStream()){
+                    new DataContractJsonSerializer(typeof(User[])).WriteObject(str, usersWithPosts);
+                    Console.WriteLine(Encoding.UTF8.GetString(str.ToArray()));
+                }
             }
         }
     }
